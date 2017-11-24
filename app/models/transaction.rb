45ed190426bb1ccb2ba6f7a57df87405851c7c39
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-23 22:52:55 -0800

    panel mostly done, onto navbar

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,5 @@
 class Transaction < ApplicationRecord
   def make_transaction
-    binding.pry
     disburser = User.find_by(id: self.disburser_id)
     collector = User.find_by(id: self.collector_id)
     disburser.credit -= self.amount
@@ -10,4 +9,17 @@ class Transaction < ApplicationRecord
     disburser.save
     collector.save
   end
+
+  def disburser_name
+    User.find_by(id: self.disburser_id).name
+  end
+
+  def collector_name
+    User.find_by(id: self.collector_id).name
+  end
+
+  def concise_date
+    (self.created_at - 25200).strftime("%b %d")
+  end
+
 end
