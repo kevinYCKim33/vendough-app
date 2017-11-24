d87a807c79944a196b59eca13d8cd2750e58a366
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-23 17:21:01 -0800

    fb login part 1 complete exclmation mark

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   def self.friends(current_user)
     where.not("id = ?", current_user.id)
