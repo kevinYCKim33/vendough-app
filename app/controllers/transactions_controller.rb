a9cd16dc61cf2585a5dea3a3ee057825389bfdbe
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-24 21:17:50 -0800

    proper bootstrap js jquery version finally found.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,16 @@
 class TransactionsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
 end
