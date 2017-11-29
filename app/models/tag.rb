8c1e4e3453c6c9d5d241502d1a927c41a0c5d9e2
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-29 00:09:47 -0800

    starting massive variable change from Transaction to Dealing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :transaction_tags
-  has_many :transactions, through: :transaction_tags
+  has_many :dealings, through: :transaction_tags, foreign_key: "tag_id", class_name: "Transaction" 
 end
--- app/models/transaction_tag.rb
+++ app/models/transaction_tag.rb
@@ -1,4 +1,4 @@
 class TransactionTag < ApplicationRecord
-  belongs_to :transaction
+  belongs_to :dealing, class_name: "Transaction", foreign_key: "transaction_id"
   belongs_to :tag
 end
