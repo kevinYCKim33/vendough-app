417b225f6bb76ac2f65946d77f5ae17d8a01dc38
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-30 23:13:54 -0800

    helper methods separated by concern

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,24 +17,4 @@ module ApplicationHelper
     'active' if current_page?(path)
   end
 
-  def format_with_color(dealing)
-    if dealing.sender == current_user && dealing.amount > 0 || dealing.recipient == current_user && dealing.amount < 0
-      "color: green; position: absolute; bottom: 48px; right: 15px; width: 150px; text-align:right;"
-    else
-      "color: red; position: absolute; bottom: 48px; right: 15px; width: 150px; text-align:right;"
-    end
-  end
-
-  def plus_or_minus(dealing)
-    if dealing.sender == current_user && dealing.amount > 0 || dealing.recipient == current_user && dealing.amount < 0
-      "+ "
-    else
-      "- "
-    end
-  end
-
-  def current_user_involved?(dealing)
-    dealing.sender == current_user || dealing.recipient == current_user
-  end
-
 end
--- app/helpers/dealings_helper.rb
+++ app/helpers/dealings_helper.rb
@@ -1,3 +1,21 @@
 module DealingsHelper
+  def format_with_color(dealing)
+    if dealing.sender == current_user && dealing.amount > 0 || dealing.recipient == current_user && dealing.amount < 0
+      "color: green; position: absolute; bottom: 48px; right: 15px; width: 150px; text-align:right;"
+    else
+      "color: red; position: absolute; bottom: 48px; right: 15px; width: 150px; text-align:right;"
+    end
+  end
 
+  def plus_or_minus(dealing)
+    if dealing.sender == current_user && dealing.amount > 0 || dealing.recipient == current_user && dealing.amount < 0
+      "+ "
+    else
+      "- "
+    end
+  end
+
+  def current_user_involved?(dealing)
+    dealing.sender == current_user || dealing.recipient == current_user
+  end
 end
