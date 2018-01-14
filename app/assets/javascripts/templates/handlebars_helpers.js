4d3454035dbc11aa9c42a55b65943fe96b02a40b
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-14 02:27:02 -0800

    plus or minus amount shown if involved ajax

--- app/assets/javascripts/templates/handlebars_helpers.js
+++ app/assets/javascripts/templates/handlebars_helpers.js
@@ -8,5 +8,17 @@ Handlebars.registerHelper('charged_or_paid', function() {
 
 Handlebars.registerHelper('concise_date', function() {
   const conciseDate = this.updated_at;
-  debugger;
+  const monthDay = new Date(conciseDate).toString().split(" ").slice(1,3).join(" ");
+  return new Handlebars.SafeString(monthDay)
+})
+
+Handlebars.registerHelper('show_amount', function() {
+  let currentUserId = parseInt($('body').attr('data-userid'));
+  if (this.sender_id === currentUserId || this.recipient_id === currentUserId) {
+    if (this.sender_id == currentUserId && this.action == "request" || this.recipient_id == currentUserId && this.action == "pay") {
+      return new Handlebars.SafeString("+ $" + Number(this.amount).toFixed(2))
+    } else {
+      return new Handlebars.SafeString("- $" + Number(this.amount).toFixed(2))
+    }
+  }
 })
