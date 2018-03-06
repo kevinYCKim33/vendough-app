29898d1a8a5e9e139180c351bbe9c3f4ef8f4880
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-03-06 01:17:31 -0800

    profile pic up on main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  def user_pic
+    if active?(root_path)
+      link_to add_fund_path do
+        image_tag current_user.avatar_url, size: "90", class: "style_main_pic"
+      end
+    else
+      image_tag current_user.avatar_url, size: "90", class: "style_main_pic"
+    end
+  end
+
   def display_credit
     number_to_currency(User.find(current_user.id).credit)
   end
